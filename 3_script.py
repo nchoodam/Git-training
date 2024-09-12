8ebc85a1e32258a64f6f163732e82bd0166ec881
nchoodam <nandakumar.choodamani@gmail.com>
2024-09-12 15:21:12 +0530

    3_script.py has been modified with addtion information

--- 3_script.py
+++ 3_script.py
@@ -3,3 +3,8 @@ print("########################################")
 print("Name of the Project is {}".format(name))
 print("########################################")
 
+
+PM_NAME = "MARK"
+print("########################################")
+print("Name of the Customer PM is {}".format(PM_NAME))
+print("########################################")
